8c3dc1e044fb0dc61d6d9353754ce467d4f36da2
mohammedahw <mohammedalihw@gmail.com>
2022-03-04 14:02:19 +0300

    finished Insertion Sort

--- src/algorithms/insertionSort.js
+++ src/algorithms/insertionSort.js
@@ -1,13 +1,29 @@
-export default function insertionSort(array) {
+export default function insertionSort(array, arrayBars) {
   let animations = []
   for (let i = 1; i < array.length; i++) {
-    let current = array[i];
-    let j;
-    for (j = i - 1; j >= 0 && array[j] > current; j--) {
-      animations.push([i, j])
-      array[j + 1] = array[j];
+    for (let j = i; j > 0; j-- ) {
+      animations.push([j, j-1])
+      if (array[j] < array[j - 1]) {
+        let temp = array[j]
+        array[j] = array[j-1] 
+        array[j-1] = temp
+      }
     }
-    array[j + 1] = current;
   }
-  return array
+  animateInseritonSort(arrayBars, animations)
+}
+
+function animateInseritonSort(arrayBars, animations) {
+  for (let i = 0; i < animations.length; i++) {
+    const [barOne, barTwo] = animations[i]
+    setTimeout(() => {
+      if (parseInt(arrayBars[barOne].style.height) < parseInt(arrayBars[barTwo].style.height)){
+        let temp = arrayBars[barOne].style.height
+        arrayBars[barOne].style.height = arrayBars[barTwo].style.height
+        arrayBars[barTwo].style.height = temp
+      }
+      arrayBars[barOne].className = "w-1 inline-block mt-0 mr-1 bg-green-800";
+      arrayBars[barTwo].className = "w-1 inline-block mt-0 mr-1 bg-blue-800";
+    }, i * 3)
+  }
 }
